d07ae2723e5cc6c595b9970dc9bed06ef43ce14d
b4rtm <bartek.matusiak12@gmail.com>
2024-04-23 12:37:06 +0200

    additional info fragment finished

--- app/src/main/java/com/example/weatherapp/fragments/AdditionalInfoFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/AdditionalInfoFragment.kt
@@ -5,25 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.R
+import com.example.weatherapp.WeatherViewModel
 
 
 class AdditionalInfoFragment : Fragment() {
 
+    private lateinit var locationTextView: TextView
+    private lateinit var humidityTextView: TextView
+    private lateinit var windSpeedTextView: TextView
+    private lateinit var windDegreeTextView: TextView
+    private lateinit var cloudsTextView: TextView
+    private lateinit var viewModel: WeatherViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_additional_info, container, false)
+        val view =  inflater.inflate(R.layout.fragment_additional_info, container, false)
+
+        viewModel = ViewModelProvider(requireActivity()).get(WeatherViewModel::class.java)
+
+        locationTextView = view.findViewById(R.id.location)
+        humidityTextView = view.findViewById(R.id.humidity)
+        windSpeedTextView = view.findViewById(R.id.windSpeed)
+        windDegreeTextView = view.findViewById(R.id.windDegree)
+        cloudsTextView = view.findViewById(R.id.clouds)
+
+        viewModel.getWeatherData().observe(viewLifecycleOwner) { weatherData ->
+            locationTextView.text = weatherData.city
+            humidityTextView.text = weatherData.humidity.toString() + "%"
+            windSpeedTextView.text = weatherData.windSpeed.toString() + " km/h"
+            windDegreeTextView.text = weatherData.windDeg.toString() + "°"
+            cloudsTextView.text = weatherData.clouds.toString() + "%"
+        }
+
+
+        return view
     }
 
 
